732cc204f9a3622344bfb599c2a43a2aafe78285
Anastasia Kuden <anastasiak@xcritical.software>
2022-08-02 14:59:40 +0300

    Add project files.

---
+++ PartsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NTS.RadioBadge.Service.Controllers.Parts
+{
+    [Route("/part")]
+    [ApiController]
+    public class PartsController : ControllerBase
+    {
+
+        #region Operations
+        [HttpGet("check")]
+        public async Task<IActionResult> CheckWheel()
+        {
+            return Ok("Все круто");
+        }
+        #endregion
+    }
+}
+
